7c08f605bce61a524ff0167e9c38c5ccc8629a7c
Bernard Carney <bernard.carney@gmail.com>
2016-04-18 17:29:53 -0600

    Updated jQuery scroll-to-anchor to account for nav on desktop-site
    Added if/else function in scroll-to-anchor.js to account for window
    width so it takes navbar into account on desktop-site

--- js/nav-scroll-change.js
+++ js/nav-scroll-change.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
-  var scroll_pos = 0;
-  
-  $(window).scroll(function () {
-    scroll_pos = $(this).scrollTop();
-    
-    if (scroll_pos > 273) {
-      $("nav ul").addClass("nav-darken");
-    }
-    else {
-      $("nav ul").removeClass("nav-darken");
-    }
-  });
+    var scroll_pos = 0;
+
+    $(window).scroll(function () {
+      scroll_pos = $(this).scrollTop();
+
+      if (scroll_pos > 400) {
+        if ($(window).width() > 500 ) { 
+          $("nav ul").addClass("nav-darken");
+        }
+      }
+      else {
+        $("nav ul").removeClass("nav-darken");
+      }
+    });
 });
