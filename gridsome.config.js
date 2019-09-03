a954d0f0934cb88dd173ef00d692e4893f63da5e
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2019-09-02 22:16:38 -0600

    added tailwind and source-files, removed default styles and boilerplate

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,28 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'BernCarney',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        route: '/blog/:year/:month/:day/:slug'
+      },
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true
+      }
+    }
+  ]
 }
