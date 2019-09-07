acc5f9e6990221012f97da01f6c247fa487087f1
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2019-09-06 21:11:57 -0600

    removed a lot of boilerplate code and added Tailwindcss

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,8 +1,8 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+class TailwindExtractor {
+  static extract (content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+  }
+}
 
 module.exports = {
   siteName: 'BernCarney',
@@ -13,16 +13,47 @@ module.exports = {
         path: 'content/posts/**/*.md',
         typeName: 'Post',
         route: '/blog/:year/:month/:day/:slug'
-      },
-      use: 'gridsome-plugin-tailwindcss',
-      options: {
-        tailwindConfig: './tailwind.config.js',
-        purgeConfig: {},
-        presetEnvConfig: {},
-        shouldPurge: true,
-        shouldImport: true,
-        shouldTimeTravel: true
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics'
+      // options: {
+      //   id: 'UA-123456789-1'
+      // }
     }
-  ]
+  ],
+
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(
+          ...[
+            require('postcss-import'),
+            require('postcss-nested'),
+            require('tailwindcss')
+          ]
+        )
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(
+            ...[
+              require('@fullhuman/postcss-purgecss')({
+                content: ['src/assets/**/*.css', 'src/**/*.vue', 'src/**/*.js'],
+                extractors: [
+                  {
+                    extractor: TailwindExtractor,
+                    extensions: ['css', 'vue', 'js']
+                  }
+                ],
+                whitelistPatterns: [/shiki/]
+              })
+            ]
+          )
+        }
+        return options
+      })
+  }
 }
