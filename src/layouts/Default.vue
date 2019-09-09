a23fabe6327bf58875dbb2c2ca83678203f07fbc
Bern Carney <bernard.j.carney@kp.org>
2019-09-09 14:09:56 -0600

    added transition for much-fancy on menu open/close

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -29,36 +29,38 @@
             </svg>
           </button>
         </div>
-        <div
-          v-show="isShowing"
-          class="w-full block flex-grow lg:flex lg:items-center lg:w-auto navlist"
-        >
-          <div class="text-sm lg:flex-grow"></div>
-          <div class="mt-12 h-screen lg:h-auto lg:mt-0">
-            <span
-              class="text-gray-700 text-l font-mono font-semibold tracking-tight mr-2 mt-4"
-              >CD</span
-            ><i class="navbar-cursor bg-gray-700 mr-6 mt-4">&nbsp;&nbsp;</i>
-            <g-link
-              to="/"
-              class="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white mr-4 uppercase"
-            >
-              Home
-            </g-link>
-            <g-link
-              to="/about"
-              class="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white mr-4 uppercase"
-            >
-              About Me
-            </g-link>
-            <g-link
-              to="/blog"
-              class="block mt-4 lg:inline-block lg:mt-0 text-gray-700 uppercase cursor-not-allowed"
-            >
-              Blog
-            </g-link>
+        <transition name="slide-fade">
+          <div
+            v-show="isShowing"
+            class="w-full block flex-grow lg:flex lg:items-center lg:w-auto navlist"
+          >
+            <div class="text-sm lg:flex-grow"></div>
+            <div class="mt-12 h-screen lg:h-auto lg:mt-0">
+              <span
+                class="text-gray-700 text-l font-mono font-semibold tracking-tight mr-2 mt-4"
+                >CD</span
+              ><i class="navbar-cursor bg-gray-700 mr-6 mt-4">&nbsp;&nbsp;</i>
+              <g-link
+                to="/"
+                class="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white mr-4 uppercase"
+              >
+                Home
+              </g-link>
+              <g-link
+                to="/about"
+                class="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-white mr-4 uppercase"
+              >
+                About Me
+              </g-link>
+              <g-link
+                to="/blog"
+                class="block mt-4 lg:inline-block lg:mt-0 text-gray-700 uppercase cursor-not-allowed"
+              >
+                Blog
+              </g-link>
+            </div>
           </div>
-        </div>
+        </transition>
       </nav>
     </header>
     <slot />
