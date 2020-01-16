2f9d5c3ffab1142b336e78f9d7104d3d93ec40f7
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-01-15 17:53:52 -0700

    tweaks

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
-	import Tailwindcss from './Tailwindcss.svelte';
-	export let name;
+  import Tailwindcss from "./Tailwindcss.svelte";
+  export let name;
 </script>
-<Tailwindcss />
-
-<main>
-	<h1 class="bg-black text-white">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 
 </style>
+
+<Tailwindcss />
+
+<main>
+  <h1 class="bg-black text-white">Hello {name}! It's me</h1>
+</main>
