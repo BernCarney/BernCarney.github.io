b867a05b319da20271b7c75ff7e8e1f7ac85967f
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-01-17 14:47:42 -0700

    added some component placeholders

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
   import Tailwindcss from './Tailwindcss.svelte';
   import Navbar from './Navbar.svelte';
+  import Hero from './Hero.svelte';
 </script>
 
 <Tailwindcss />
 
 <Navbar />
+
+<Hero />
--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <header>
   <nav
-    class="flex items-center justify-between flex-wrap pt-3 pb-4 px-4
+    class="flex items-center justify-between flex-wrap pt-1 pb-4 px-4
     border-red-600 border-t-4"
   >
     <!-- Logo -->
@@ -31,28 +31,28 @@
     >
       <div class="text-sm uppercase lg:flex-grow">
         <a
-          href="#responsive-header"
+          href="#home"
           class="block mt-8 lg:inline-block lg:mt-0 text-white font-semibold
           border-b-4 border-transparent hover:border-red-600 mr-4"
         >
           Home
         </a>
         <a
-          href="#responsive-header"
+          href="#about"
           class="block mt-8 lg:inline-block lg:mt-0 text-white font-semibold
           border-b-4 border-transparent hover:border-red-600 mr-4"
         >
           About
         </a>
         <a
-          href="#responsive-header"
+          href="#articles"
           class="block mt-8 lg:inline-block lg:mt-0 text-white font-semibold
           border-b-4 border-transparent hover:border-red-600 mr-4"
         >
           Articles
         </a>
         <a
-          href="#responsive-header"
+          href="#projects"
           class="block mt-8 lg:inline-block lg:mt-0 text-white font-semibold
           border-b-4 border-transparent hover:border-red-600 mr-4"
         >
@@ -60,7 +60,7 @@
         </a>
 
         <a
-          href="#"
+          href="#contact"
           class="inline-block text-sm px-4 py-2 leading-none border text-white
           border-white font-semibold hover:border-transparent hover:text-red-600
           hover:bg-white mt-8 lg:mt-0"
