fda717e242b67f428a68dffb50301f5dc2a78d54
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-01-18 13:58:18 -0700

    initial mockup of weird box stuff

---
+++ src/Hero.svelte
@@ -0,0 +1,7 @@
+<div class="flex w-full min-h-screen absolute top-0">
+  <div class="flex w-2/4">
+    <div class="w-1/3">&nbsp;</div>
+    <div class="bg-red-600 w-2/3 shadow-xl">&nbsp;</div>
+  </div>
+  <div class="w-2/4">&nbsp;</div>
+</div>
